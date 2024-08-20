db1be617597d4505e4deb1e8b7dede5d52e0963c
fatimasiyal92 <110052033+fatimasiyal92@users.noreply.github.com>
2024-08-20 11:12:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 redis_host=os.getenv('REDIS_HOST', 'localhost')
-redis_password= os.getenv('REDIS_PASSWORD', 'None')
+redis_password= os.getenv('REDIS_PASSWORD', None)
 redis = Redis(host='redis_host', port=6379, password='redis_password')
 
 @app.route('/')
